a22a52f13fd56be7eeb19794d98f3230a03a2702
Charulata Kieszkiewicz <charulatapatel2002@yahoo.co.uk>
2022-04-25 19:58:39 +0100

    feat: add Hand class , checkCanCreateHand in PokerHandsTest class

---
+++ src/main/java/com/techreturners/model/Hand.java
@@ -0,0 +1,23 @@
+package com.techreturners.model;
+import java.util.ArrayList;
+
+public class Hand {
+
+   private ArrayList<Card> cards = new ArrayList<>(5);
+
+    public Hand (String[] input){
+        for (int i = 0; i < input.length; i++) {
+            this.cards.add(new Card(input[i]));
+        }
+
+    }
+
+
+    public int getValue(int i) {
+        return this.cards.get(i).getValue();
+    }
+
+    public char getSuit(int i) {
+        return this.cards.get(i).getSuit();
+    }
+}
--- src/test/java/PokerHandsTest.java
+++ src/test/java/PokerHandsTest.java
@@ -1,6 +1,9 @@
+import com.techreturners.model.Hand;
+import com.techreturners.model.Card;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import com.techreturners.model.Card;
+import org.junit.jupiter.api.Disabled;
+
 
 public class PokerHandsTest {
 
@@ -68,6 +71,28 @@ public class PokerHandsTest {
         //Assert and Act
         Assertions.assertEquals(valueExpected, card.getValue());
         Assertions.assertEquals(suitExpected, card.getSuit());
-
     }
+
+    @Test
+    public void checkCanCreateHand(){
+        //Arrange
+        String [] input = {"2H", "3D", "5S", "9C", "KD"};
+
+        //Further Arrange
+        Hand hand = new Hand(input);
+
+        //Assert and Act
+        Assertions.assertEquals(2, hand.getValue(0));
+        Assertions.assertEquals('H', hand.getSuit(0));
+        Assertions.assertEquals(3, hand.getValue(1));
+        Assertions.assertEquals('D', hand.getSuit(1));
+        Assertions.assertEquals(5, hand.getValue(2));
+        Assertions.assertEquals('S', hand.getSuit(2));
+        Assertions.assertEquals(9, hand.getValue(3));
+        Assertions.assertEquals('C', hand.getSuit(3));
+        Assertions.assertEquals(13, hand.getValue(4));
+        Assertions.assertEquals('D', hand.getSuit(4));
+    }
+
+    
 }
